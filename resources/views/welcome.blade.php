0d8891e48dfbdf8876b46dfde4258cefc86c3012
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2020-03-08 22:06:26 +0200

    frist commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+      <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        
+            <a class="navbar-brand"   href="#">Digital Media Footprint</a>
+        
+                <a class="nav-link" href="#">Clients</a>
+                <a class="nav-link" href="#">Services Provided</a>
+      </nav>
+
+
+         <div class="" style="text-align: center;background-color: #e3f2fd;">
+         &copy; DS 2020
+         </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+
+    </body>
+</html>
